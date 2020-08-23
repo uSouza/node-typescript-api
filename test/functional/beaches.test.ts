1e0d702041f144f53374764cf261df37d6d222f3
Uesley Silva <uesley.silva@dotgroup.com.br>
2020-08-23 20:56:53 -0300

    Integrate user in application models

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,7 +1,20 @@
-import { Beach } from "@src/models/beach";
+import { Beach } from '@src/models/beach';
+import { User } from '@src/models/user';
+import { AuthService } from '@src/services/auth';
 
 describe('Beaches functional tests', () => {
-  beforeAll(async () => Beach.deleteMany({}));
+  const defaultUser = {
+    name: 'John Doe',
+    email: 'john@mailinator.com',
+    password: '123456',
+  };
+  let token: string;
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+    await User.deleteMany({});
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
+  });
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -11,7 +24,10 @@ describe('Beaches functional tests', () => {
         position: 'E',
       };
 
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
@@ -23,10 +39,14 @@ describe('Beaches functional tests', () => {
         position: 'E',
       };
 
-      const response = await global.testRequest.post('/beaches').send(newBeach);
+      const response = await global.testRequest
+        .post('/beaches')
+        .set({ 'x-access-token': token })
+        .send(newBeach);
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
-        error: 'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
       });
     });
     it.skip('should return 500 when there is any error other than validation error', async () => {
