f6dab38297a816410c6c9f2af11c1b4fa02b0e74
Uesley Silva <uesleysilva@alunos.utfpr.edu.br>
2020-09-07 10:51:32 -0300

    Implement order by rating in forecast list

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -9,7 +9,7 @@ jest.mock('@src/clients/stormGlass');
 
 describe('Forecast Service', () => {
   const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
-  it('should return the forecast for mutiple beaches in the same hour with different ratings', async () => {
+  it('should return the forecast for mutiple beaches in the same hour with different ratings ordered by rating', async () => {
     mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
       {
         swellDirection: 123.41,
@@ -56,21 +56,6 @@ describe('Forecast Service', () => {
         forecast: [
           {
             lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100,
-          },
-          {
-            lat: -33.792726,
             lng: 141.289824,
             name: 'Dee Why',
             position: 'S',
@@ -84,6 +69,21 @@ describe('Forecast Service', () => {
             windDirection: 299.45,
             windSpeed: 100,
           },
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2020-04-26T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100,
+          }
         ],
       },
     ];
